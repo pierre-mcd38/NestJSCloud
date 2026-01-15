ff075fadcf6ae3863af96f0f7366d464e83bfbf6
Pierre <pierre.Michaud3806@gmail.com>
2026-01-15 14:52:59 +0100

    Ajout de l'UI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { GamesModule } from './games/games.module';
 
 @Module({
-  imports: [GamesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    GamesModule,
+  ],
 })
 export class AppModule {}
